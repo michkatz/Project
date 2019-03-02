95f833cf8bf2e2d30126a2ee7d435ea0d44a6a5b
Andrew Copsey <copsea@uw.edu>
2019-03-02 11:41:05 -0800

    adding code section to project, including __init__.py file, test functions file and data folder

---
+++ xrdos/__init__.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from .xrdos import *
+#etc, all the import all functions and packages said functions run on to 
+#be used at the highest level namespace.
---
+++ xrdos/tests/__init__.py
@@ -0,0 +1,3 @@
+# import all of the python files + funcions from them which need to be 
+# tested, one test file for each .py file we have
+# use command 'pytest mymodule' in top level directory to run tests
