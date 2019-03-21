1c92789bd77df91b6140169b1da35e6582971ffc
Corey Johnson <crj14@uw.edu>
2019-03-21 12:44:42 -0700

    renamed retrieval functions

--- xrdos/tests/test_mpd_data_retrieval.py
+++ xrdos/tests/test_mpd_data_retrieval.py
@@ -1,29 +1,24 @@
 import pandas as pd
-from xrdos import MPD_Data_Retrieval as MPD
+from xrdos import mpd_data_retrieval as MPD
 import os.path
-
 
 def test_Retrieve_data():
     MPD.Retrieve_data(0, 0.001, 'raw')
     assert os.path.isfile('./raw.csv') is True
-
 
 def test_Extract_data():
     df = pd.read_csv('raw.csv', sep='\t')
     results = MPD.Extract_data(df.iloc[0])
     assert isinstance(results, pd.DataFrame)
 
-
 def test_Reformat_data():
     df = pd.read_csv('raw.csv', sep='\t')
     results = MPD.Reformat_data(df.iloc[0])
     assert isinstance(results, dict)
 
-
 def test_Produce_data():
     MPD.Produce_data('raw', 'processed')
     assert os.path.isfile('./processed.csv') is True
-
 
 def test_MPD_Data():
     results = MPD.MPD_Data(0, 0.001, 'raw', 'processed')
